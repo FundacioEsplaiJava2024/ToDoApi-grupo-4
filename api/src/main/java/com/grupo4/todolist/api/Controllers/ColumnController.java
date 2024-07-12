124a767804d9a6d1694013c46d35a9b39b2b3920
JordiJT99 <jordijt.99@gmail.com>
2024-07-12 11:07:41 +0200

    se añade funcion de eliminar columna

--- api/src/main/java/com/grupo4/todolist/api/Controllers/ColumnController.java
+++ api/src/main/java/com/grupo4/todolist/api/Controllers/ColumnController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,16 @@ public class ColumnController {
             }
             columna.get().setNombre(nombreCol.getNombre());
             return ResponseEntity.status(HttpStatus.CREATED).body(serviceColumn.guardarColumnas(nombreCol));
-        } 
         }
+        
+        //Eliminar columna
+        @DeleteMapping("del/{id}")
+        public ResponseEntity<Columnas> delete(@PathVariable Long id){
+            if(!serviceColumn.findById(id).isPresent()){//si no se encuentra la columna devuelve el codigo 404
+                return ResponseEntity.notFound().build();
+            }
+            serviceColumn.deletebyId(id);
+            return ResponseEntity.ok().build();
+        }
+}
 
