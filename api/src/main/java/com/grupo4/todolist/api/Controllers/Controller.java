b4520e4db848e645da5379f084536e189113ee03
Ney Apolinario <ney_apolinario1@hotmail.com>
2024-07-12 10:07:41 +0200

    config

--- api/src/main/java/com/grupo4/todolist/api/Controllers/Controller.java
+++ api/src/main/java/com/grupo4/todolist/api/Controllers/Controller.java
@@ -6,18 +6,14 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import com.google.gson.Gson;
+import com.grupo4.todolist.api.Model.Entities.Task;
 import com.grupo4.todolist.api.Model.Greeting;
 import com.grupo4.todolist.api.Model.TaskModel;
-import com.grupo4.todolist.api.Model.Entities.Task;
-import com.grupo4.todolist.api.Model.Repositories.TaskRepo;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -49,7 +45,7 @@ public class Controller {
 
 		int cont = model.addTask(entity);
 		
-		if (cont == 1) {
+		if (cont == 1){
 			response = "Tarea agregada correctamente";
 		}else if(cont == 0){
 			response = "Error al agregar tarea";
