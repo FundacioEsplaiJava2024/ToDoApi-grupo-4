67bf17a7f14f67b670779d551f74bc8d8eb5a9f4
JordiJT99 <jordijt.99@gmail.com>
2024-07-17 10:52:11 +0200

    se añade base de datos en properties para que la información del postman se duplique en la base de datos

--- api/src/main/java/com/grupo4/todolist/api/Model/Repositories/RepositoryColumn.java
+++ api/src/main/java/com/grupo4/todolist/api/Model/Repositories/RepositoryColumn.java
@@ -2,6 +2,7 @@ package com.grupo4.todolist.api.Model.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.grupo4.todolist.api.Model.Entities.Columnas;
 
 @Repository
