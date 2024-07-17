d69a0053514c11858a33b3d627eaa6bc79b07965
AleXKiD31 <alexkid20025@gmail.com>
2024-07-17 11:50:05 +0200

    UserController

---
+++ api/src/main/java/com/grupo4/todolist/api/Controllers/UserController.java
@@ -0,0 +1,41 @@
+package com.grupo4.todolist.api.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.grupo4.todolist.api.Domain.UserService;
+import com.grupo4.todolist.api.Domain.Entities.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService UserService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return UserService.createUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return UserService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return UserService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return UserService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        UserService.deleteUser(id);
+    }
+}
---
+++ api/src/main/java/com/grupo4/todolist/api/Domain/Entities/User.java
@@ -0,0 +1,58 @@
+package com.grupo4.todolist.api.Domain.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_ID;
+
+    private String user_name;
+
+    private String user_password;
+
+    private String user_email;
+
+    public User() {}
+
+    public User(long user_ID, String user_name, String user_password, String user_email) {
+        this.user_ID =  user_ID;
+        this.user_name = user_name;
+        this.user_password = user_password;
+        this.user_email = user_email;
+    }
+
+    public String getName() {
+        return user_name;
+    }
+    public long getID() {
+        return user_ID;
+    }
+    public String getPassword() {
+        return user_password;
+    }
+    public String getEmail() {
+        return user_email;
+    }
+    
+    public void setName(String user_name) {
+        this.user_name = user_name;
+    }    
+    public void setID(long user_ID) {
+        this.user_ID = user_ID;
+    }    
+    public void setPassword(String user_password) {
+        this.user_password = user_password;
+    }    
+    public void setEmail(String user_email) {
+        this.user_email = user_email;
+    }
+}
